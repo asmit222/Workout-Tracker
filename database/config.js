684cb50140db05d35215c7f1e9828baa97286b10
Austin Smith <asmit2222@gmail.com>
2020-08-18 18:46:01 -0400

    database config update for heroku

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: 'austin',
-  password: 'pw',
-  host: 'localhost',
+  user: 'b96f58aac16246',
+  password: 'de0ac6a3',
+  host: 'us-cdbr-east-02.cleardb.com',
   database: 'workout'
 };
