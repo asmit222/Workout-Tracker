3af315bcf9a1edb20da0a74b098b2f25959f2040
Austin Smith <asmit2222@gmail.com>
2020-08-19 08:06:17 -0400

    changed config for database

--- database/config.js
+++ database/config.js
@@ -2,5 +2,5 @@ module.exports = {
   user: 'b2f7b55503f0b9',
   password: 'acec938e',
   host: 'us-cdbr-east-02.cleardb.com',
-  database: 'heroku_f36147dde8615f2'
+  database: 'heroku_1234d80025b881f'
 };
