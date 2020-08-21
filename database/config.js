45b856a966c5ddf44b4bf90d33cf501d946eb1c3
Austin Smith <asmit2222@gmail.com>
2020-08-20 23:20:31 -0400

    updated config

--- database/config.js
+++ database/config.js
@@ -2,19 +2,19 @@ const { Pool } = require('pg');
 const mariadb = require("mariadb/callback");
 
 const config =  {
-user: 'b2f7b55503f0b9',
-password: 'acec938e',
+user: 'b9cf24c448af3c',
+password: '9a5204d2',
 host: 'us-cdbr-east-02.cleardb.com',
-database: 'heroku_1234d80025b881f',
+database: 'heroku_48dcf701ba22c78',
 port: 3306
 };
 
 var pool      =    mariadb.createPool({
   connectionLimit : 10,
   host     : 'us-cdbr-east-02.cleardb.com',
-  user     : 'b2f7b55503f0b9',
-  password : 'acec938e',
-  database : 'heroku_1234d80025b881f',
+  user     : 'b9cf24c448af3c',
+  password : '9a5204d2',
+  database : 'heroku_48dcf701ba22c78',
   debug    :  false
 });
 
