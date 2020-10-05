6384379ddca9dfdbfdc7e4c9f7be6afce20962b2
Austin Smith <asmit2222@gmail.com>
2020-10-05 08:27:02 -0400

    hopefully installable

--- client/index.js
+++ client/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+const path = require("path");
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+if ('serviceWorker' in navigator) {
+     window.addEventListener('load', () => {
+       navigator.serviceWorker.register(path.resolve(__dirname, '../dist', 'service-worker.js'))
+      .then(registration => {
+         console.log('SW registered: ', registration);
+       }).catch(registrationError => {
+         console.log('SW registration failed: ', registrationError);
+      });
+     });
+   }
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./client/index.js",
@@ -28,6 +31,32 @@ module.exports = {
 
     ],
   },
+    plugins: [
+      new HtmlWebpackPlugin({
+       title: 'Eazy-Trak',
+       title: 'Eazy-Trak',
+        template: path.resolve('public/index.html')
+      }),
+      new CopyWebpackPlugin(
+        {
+       patterns: [
+         { from: 'public/dist/manifest', to: 'manifest' }
+        ]
+      }
+      ),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+    swDest: 'service-worker.js',
+    clientsClaim: true,
+    skipWaiting: true,
+    runtimeCaching: [{
+      urlPattern: new RegExp('http://localhost:2020/'),
+      handler: 'StaleWhileRevalidate'
+    }]
+
+    }),
+    ],
   output: {
     path: path.resolve(__dirname, "public/dist"),
     filename: "bundle.js",
