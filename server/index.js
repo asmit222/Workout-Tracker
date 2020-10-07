3d7b4662e5bc008b1fd300c4c7be634907c35d8a
Austin Smith <asmit2222@gmail.com>
2020-10-07 15:53:52 -0400

    test

--- server/index.js
+++ server/index.js
@@ -14,14 +14,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + '/../public')));
 
 
-// app.use((req, res, next) => {
-//   if (req.header('x-forwarded-proto') !== 'https') {
-//     console.log('info: ', req.header, req.url)
-//     res.redirect(`https://${req.header('host')}${req.url}`)
-//   } else {
-//     next()
-//   }
-// })
+//=======supposed to redirect to https==============
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    console.log('info: ', req.header, req.url)
+    res.redirect(`https://${req.header('eazy-trak')}${req.url}`)
+  } else {
+    next()
+  }
+})
 
 
 app.listen(PORT, function() {
